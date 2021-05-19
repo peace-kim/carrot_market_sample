3460b3312af0963ee65f79aaf2e5798b3e60753f
peacewillpeace <peacewillpeace@gmail.com>
2021-05-19 18:03:58 +0900

    commit on 19th May, with lecture 10

--- lib/page/detail.dart
+++ lib/page/detail.dart
@@ -1,4 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:carrot_market_sample/components/manor_temperature.dart';
 import 'package:flutter/material.dart';
 
 class DetailContentView extends StatefulWidget {
@@ -12,20 +13,22 @@ class DetailContentView extends StatefulWidget {
 
 class _DetailContentViewState extends State<DetailContentView> {
   Size size;
-  List<Map<String,String>> imgList;
+  List<Map<String, String>> imgList;
   int _current;
 
   @override
   void didChangeDependencies() {
     // TODO: implement didChangeDependencies
     super.didChangeDependencies();
-    size = MediaQuery.of(context).size;
+    size = MediaQuery
+        .of(context)
+        .size;
     imgList = [
-      {"id" : "0", "url" : widget.data["image"]},
-      {"id" : "1", "url" : widget.data["image"]},
-      {"id" : "2", "url" : widget.data["image"]},
-      {"id" : "3", "url" : widget.data["image"]},
-      {"id" : "4", "url" : widget.data["image"]},
+      {"id": "0", "url": widget.data["image"]},
+      {"id": "1", "url": widget.data["image"]},
+      {"id": "2", "url": widget.data["image"]},
+      {"id": "3", "url": widget.data["image"]},
+      {"id": "4", "url": widget.data["image"]},
     ];
   }
 
@@ -33,9 +36,11 @@ class _DetailContentViewState extends State<DetailContentView> {
     return AppBar(
       backgroundColor: Colors.transparent,
       elevation: 0,
-      leading: IconButton(onPressed: (){
-        Navigator.pop(context);
-      },icon: Icon(Icons.arrow_back, color: Colors.white)),
+      leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back, color: Colors.white)),
       actions: [
         IconButton(onPressed: () {}, icon: Icon(Icons.share)),
         IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
@@ -43,70 +48,112 @@ class _DetailContentViewState extends State<DetailContentView> {
     );
   }
 
-  Widget _bodyWidget() {
-    return Stack(
-      children: [
-        Hero(
-          tag: widget.data["cid"],
-          child: Container(
-            child:
-            CarouselSlider(
-              options: CarouselOptions(
-                height: size.width,
-                initialPage: 0,
-                enableInfiniteScroll: false,
-                viewportFraction: 1,
-                onPageChanged: (index, reason){
-                  setState((){
-                    _current = index;
-                    print(index);
-                  });
-                }
+  Widget _makeSliderImage() {
+    return Container(
+      child: Stack(
+        children: [
+          Hero(
+            tag: widget.data["cid"],
+            child: Container(
+              child: CarouselSlider(
+                options: CarouselOptions(
+                    height: size.width,
+                    initialPage: 0,
+                    enableInfiniteScroll: false,
+                    viewportFraction: 1,
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        _current = index;
+                        print(index);
+                      });
+                    }),
+                items: imgList.map((map) {
+                  return Image.asset(map["url"],
+                      width: size.width, fit: BoxFit.fill);
+                }).toList(),
               ),
-              items: imgList.map((map){
-              return Image.asset(
-                    map["url"],
-                    width: size.width,
-                    fit: BoxFit.fill
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: imgList.map((map) {
+                return Container(
+                  width: 10.0,
+                  height: 10.0,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: _current == int.parse(map["id"])
+                        ? Colors.white
+                        : Colors.white.withOpacity(0.4),
+                  ),
                 );
               }).toList(),
-
             ),
-
-
           ),
-        ),
-        
-        Positioned(
-          bottom: 0,
-          left: 0,
-          right: 0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: imgList.map((map) {
-              return Container(
-                width: 10.0,
-                height: 10.0,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: _current == int.parse(map["id"])
-                      ? Colors.white
-                      : Colors.white.withOpacity(0.4),
+        ],
+      ),
+    );
+  }
+
+  Widget _sellerSimpleInfo() {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Row(
+        children: [
+          // ClipRRect(
+          //   borderRadius: BorderRadius.circular(50),
+          //   child: Container(
+          //     width: 50, height: 50, child: Image.asset("assets/images/user.png"),
+          //   ),
+          // )
+          CircleAvatar(
+            radius: 25,
+            backgroundImage: Image
+                .asset("assets/images/user.png")
+                .image,
+          ),
+          SizedBox(width: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '판매자 개남',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  // color: Colors.black,
                 ),
-              );
-            }).toList(),
+              ),
+              Text('제주시 도담동',)
+            ],
           ),
-        ),
+          Expanded(
+            child: ManorTemperature(
+              manorTemp: 37.5,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
-
+  Widget _bodyWidget() {
+    return Column(
+      children: [
+        _makeSliderImage(),
+        _sellerSimpleInfo(),
       ],
     );
   }
 
-  Widget _bottomBarWidget(){
+  Widget _bottomBarWidget() {
     return Container(
-      width : size.width,
+      width: size.width,
       height: 55,
       color: Colors.red,
     );
